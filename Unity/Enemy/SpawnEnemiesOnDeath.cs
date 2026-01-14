1df0f7b08565dca53b78ada26094ae70a3794d0a
Julian Federico Viel <111827169+Eilifir@users.noreply.github.com>
2026-01-13 23:13:32 -0300

    Create SpawnEnemiesOnDeath.cs

---
+++ Unity/Enemy/SpawnEnemiesOnDeath.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnEnemiesOnDeath : MonoBehaviour
+{
+    [SerializeField] GameObject enemyToSpawn;
+    EnemyHandler mainScript;
+    private void Start()
+    {
+        mainScript = GetComponent<EnemyHandler>();
+    }
+    private void OnDestroy()
+    {
+        GameObject enemySpawned = Instantiate(miniSlimes, transform.position, Quaternion.identity);
+        EnemyHandler enemySpawned = enemySpawned.GetComponent<EnemyHandler>();
+        slimeOneScript.currentWaypoint =  mainScript.currentWaypoint;
+    }
+}
